234ac09134ae5ef6ff6115319ef03e1a50f1b7fc
Sasipasch <sasipas.ch@chococrm.com>
2023-01-24 10:34:31 +0700

    config: parser vue, parserOptions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ module.exports = {
     env: {
         node: true,
     },
-    parser: '@typescript-eslint/parser',
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
     plugins: ['@typescript-eslint'],
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
 }
