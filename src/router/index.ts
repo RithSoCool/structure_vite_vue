db9a629710ac1ebfeb4aa0510a461eb9245b8380
Jirasin <jirasin.si@chococrm.com>
2023-01-25 11:35:32 +0700

    [Adjust] Register - Init file & add router-view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,23 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
-const routes = []
+import HelloWorldVue from '../components/HelloWorld.vue'
+import RegisterMobile from '../views/register/RegisterMobile.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: HelloWorldVue,
+        name: 'hello',
+    },
+    {
+        path: '/register-mobile',
+        component: RegisterMobile,
+        name: 'Register Mobile',
+    },
+]
 
 export default createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes,
     scrollBehavior() {
         // always scroll to top
