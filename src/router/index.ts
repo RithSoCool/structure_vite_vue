96d02432cf397e58fc3eef8cb5b45a6466286b65
Jirasin <jirasin.si@chococrm.com>
2023-01-26 14:48:06 +0700

    [Adjust] Router - split router to folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HelloWorldVue from '../components/HelloWorld.vue'
-import RegisterMobile from '../views/register/RegisterMobile.vue'
-import MainTab from '../views/main/MainTab.vue'
-import MainPage from '../views/main/MainPage.vue'
-import PointPage from '../views/main/PointPage.vue'
-import ProfilePage from '../views/main/ProfilePage.vue'
+import { main } from './main'
+import { register } from './register'
+
 const routes = [
     {
         path: '/',
@@ -17,53 +15,8 @@ const routes = [
             duration: 200,
         },
     },
-    {
-        path: '/register-moblie',
-        component: RegisterMobile,
-        name: 'Register Mobile',
-        meta: {
-            entry: 'center',
-            exit: 'right',
-        },
-    },
-    {
-        path: '/main',
-        component: MainTab,
-        name: 'Main Tab',
-        meta: {
-            entry: 'center',
-            exit: 'right',
-        },
-        children: [
-            {
-                path: '/main/profile',
-                component: ProfilePage,
-                name: 'profile',
-                meta: {
-                    title: 'โปรไฟล์',
-                    show: true,
-                },
-            },
-            {
-                path: '/main/point',
-                component: PointPage,
-                name: 'point',
-                meta: {
-                    title: 'สะสมคะแนน',
-                    show: true,
-                },
-            },
-            {
-                path: '',
-                component: MainPage,
-                name: 'main',
-                meta: {
-                    title: 'หน้าหลัก',
-                    show: true,
-                },
-            },
-        ],
-    },
+    ...main,
+    ...register,
 ]
 
 export default createRouter({
---
+++ src/router/register/index.ts
@@ -0,0 +1,13 @@
+import RegisterMobile from '../../views/register/RegisterMobile.vue'
+
+export const register = [
+    {
+        path: '/register-moblie',
+        component: RegisterMobile,
+        name: 'Register Mobile',
+        meta: {
+            entry: 'center',
+            exit: 'right',
+        },
+    },
+]
